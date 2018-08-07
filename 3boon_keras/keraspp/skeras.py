e7c4305599b94f71c066622c13690fa2f73a419e
songjein <jeinsong100@gmail.com>
2018-08-07 14:01:35 +0900

    DNN

---
+++ 3boon_keras/keraspp/skeras.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+def plot_loss(history):
+    plt.plot(history.history['loss']) # 선그리기
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc=0) # 각 라인의 표식 표시
+
+def plot_acc(history):
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc=0)
